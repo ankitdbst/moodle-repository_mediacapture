bddd1ff071784bb5e284ae608af0700724603e54
ANKIT GUPTA <mailtoankitgupta@gmail.com>
2012-05-14 14:21:59 +0530

    Added additional language strings for en.

--- lang/en/repository_mediacapture.php
+++ lang/en/repository_mediacapture.php
@@ -20,5 +20,25 @@
 $string['pluginname'] = 'Media Capture';
 $string['mediacapture:view'] = 'Use Media Capture in file picker';
 $string['configplugin'] = 'Media Capture repository configuration';
-$string['account'] = 'Account information';
-$string['api_key'] = 'API Key';
+$string['repositoryname'] = 'MediaCapture';
+
+$string['audio_format'] = 'Audio format';
+$string['audio_format_imaadpcm'] = 'ImaADPCM';
+$string['audio_format_speex'] = 'Speex';
+
+$string['sampling_rate'] = 'Sampling rate';
+$string['sampling_rate_low'] = 'Low quality';
+$string['sampling_rate_medium'] = 'Medium quality';
+$string['sampling_rate_normal'] = 'Normal quality';
+$string['sampling_rate_high'] = 'High quality';
+
+$string['recordnew'] = 'New recording';
+$string['unexpectedevent'] = 'An unexpected event was detected. Please report this problem to the Moodle developers.';
+$string['appletnotfound'] = 'The applet was not detected. Please make sure that you have Java Runtime Environment (JRE) installed and the applet loaded properly.';
+$string['norecordingfound'] = 'There is no sound recorded. Please use the red disc button to start recording. Use the black square button to stop recording.';
+$string['nonamefound'] = 'Please provide a name for your recording.';
+$string['javanotfound'] = 'Your browser does not have Java support. Consider installing the latest version of <a href="http://www.java.com/en/download/">Java Runtime Engine</a>';
+
+$string['save'] = 'Save';
+$string['name'] = 'Name';
+$string['account'] = 'Account';
